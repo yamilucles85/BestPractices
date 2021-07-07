28437d85e97a0f7538b2f173b31ee87092a0cacc
yamilucles85 <yamilucles85@gmail.com>
2021-07-07 07:59:53 -0600

    Add project files.

---
+++ Prestige Biz/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestige.Biz
+{
+    public class Actor
+    {
+        /// <summary>
+        /// Will return title
+        /// </summary>
+        /// <returns></returns>
+        public string GetOccupation() 
+        {
+            return "Actor";
+        }
+    }
+}
---
+++ Prestige.BizTests/ActorTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Prestige.Biz;
+
+namespace Prestige.BizTests
+{
+    [TestClass]
+    public class ActorTest
+    {
+        [TestMethod]
+        public void TestGetOccupation()
+        {
+            // Arrange
+            var currentActor = new Actor();
+            var expected = "Actor";
+
+            // Act
+            string result = currentActor.GetOccupation();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
